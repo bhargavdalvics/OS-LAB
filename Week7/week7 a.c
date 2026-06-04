182b4a89ab8709159920a5f1811b5ff1f7a93b20
bhargavdalvics <bhargavdalvi.cs24@bmsce.ac.in>
2026-06-05 00:50:22 +0530

    Update week7 a.c

--- Week7/week7 a.c
+++ Week7/week7 a.c
@@ -5,24 +5,30 @@ typedef struct {
     int id;
     int execution_time;
     int period;
-    int deadline;
+    int deadline;     //deadline of current task
     int remaining_time;
 } Task;
 
-void rate_monotonic(Task tasks[], int n, int hyperperiod) {
+void rate_monotonic(Task tasks[], int n, int hyperperiod) {  //hyperperiod-total simulation time
     printf("\n--- Rate Monotonic Scheduling ---\n");
-    for (int t = 0; t < hyperperiod; t++) {
-        int chosen = -1;
+    for (int t = 0; t < hyperperiod; t++) {    //t-current time unit
+        int chosen = -1;       
+        //chosen-Stores the index of the selected task.
+        //-1 means no task selected
+        
         for (int i = 0; i < n; i++) {
-            if (t % tasks[i].period == 0) {
-                tasks[i].remaining_time = tasks[i].execution_time;
-                tasks[i].deadline = t + tasks[i].period;
+            //New Task Arrival
+            if (t % tasks[i].period == 0) {    //Checks whether a new instance of the task arrives.
+                tasks[i].remaining_time = tasks[i].execution_time; //reloads execution time
+                tasks[i].deadline = t + tasks[i].period; //sets the deadline
             }
+            //Priority Selection
             if (tasks[i].remaining_time > 0) {
-                if (chosen == -1 || tasks[i].period < tasks[chosen].period)
+                if (chosen == -1 || tasks[i].period < tasks[chosen].period)  //smallest period = highest priority 
                     chosen = i;
             }
         }
+        //Execute Selected Task
         if (chosen != -1) {
             tasks[chosen].remaining_time--;
             printf("Time %d: Task %d running\n", t, tasks[chosen].id);
