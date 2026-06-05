2061c4b44c20e83cb22d55cb61ba79937194cdbd
bhargavdalvics <bhargavdalvi.cs24@bmsce.ac.in>
2026-06-05 19:50:51 +0530

    Update firstfit.c

--- WEEK9/firstfit.c
+++ WEEK9/firstfit.c
@@ -2,20 +2,33 @@
 
 int main() {
     int nb, np;
-    printf("Blocks: "); scanf("%d", &nb);
+    printf("Blocks: ");
+    scanf("%d", &nb);
     int b[nb];
-    for(int i=0;i<nb;i++){ printf("B%d: ",i+1); scanf("%d",&b[i]); }
+    for(int i=0;i<nb;i++){
+        printf("B%d: ",i+1); 
+        scanf("%d",&b[i]);
+    }
 
-    printf("Processes: "); scanf("%d", &np);
+    printf("Processes: "); 
+    scanf("%d", &np);
     int p[np], a[np];
-    for(int i=0;i<np;i++){ printf("P%d: ",i+1); scanf("%d",&p[i]); a[i]=-1; }
+    for(int i=0;i<np;i++){ 
+        printf("P%d: ",i+1); 
+        scanf("%d",&p[i]);
+        a[i]=-1; }
 
     for(int i=0;i<np;i++)
         for(int j=0;j<nb;j++)
-            if(b[j]>=p[i]){ a[i]=j; b[j]-=p[i]; break; }
+            if(b[j]>=p[i]){ 
+                a[i]=j; 
+                b[j]-=p[i];
+                break;
+            }
 
     printf("\nProc\tSize\tBlock\n");
     for(int i=0;i<np;i++)
-        if(a[i]!=-1) printf("%d\t%d\t%d\n",i+1,p[i],a[i]+1);
+        if(a[i]!=-1) 
+            printf("%d\t%d\t%d\n",i+1,p[i],a[i]+1);
         else printf("%d\t%d\tNA\n",i+1,p[i]);
 }
