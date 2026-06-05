c40868d8f86220b72089077e1312a58663e9c26e
bhargavdalvics <bhargavdalvi.cs24@bmsce.ac.in>
2026-06-05 21:53:29 +0530

    Update bestfit.c

--- WEEK9/bestfit.c
+++ WEEK9/bestfit.c
@@ -22,12 +22,18 @@ int main() {
     for(int i=0;i<np;i++){
         int best=-1;
         for(int j=0;j<nb;j++)
-            if(b[j]>=p[i] && (best==-1 || b[j]<b[best])) best=j; //Save the index of the current best block.
-        if(best!=-1){ alloc[i]=best; b[best]-=p[i]; }     
+            if(b[j]>=p[i] && (best==-1 || b[j]<b[best])) 
+                best=j; //Save the index of the current best block.
+        if(best!=-1){ 
+            alloc[i]=best; 
+            b[best]-=p[i];
+        }     
     }
 
     printf("\nProcess\tSize\tBlock\n");
     for(int i=0;i<np;i++)
-        if(alloc[i]==-1) printf("%d\t%d\tNot Allocated\n", i+1, p[i]);
-        else printf("%d\t%d\t%d\n", i+1, p[i], alloc[i]+1);
+        if(alloc[i]==-1) 
+            printf("%d\t%d\tNot Allocated\n", i+1, p[i]);
+        else 
+            printf("%d\t%d\t%d\n", i+1, p[i], alloc[i]+1);
 }
