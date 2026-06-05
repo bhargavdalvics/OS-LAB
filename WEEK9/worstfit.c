a3369df478f0d662408211b16b609242aa8b88e8
bhargavdalvics <bhargavdalvi.cs24@bmsce.ac.in>
2026-06-05 19:52:02 +0530

    Update worstfit.c

--- WEEK9/worstfit.c
+++ WEEK9/worstfit.c
@@ -6,23 +6,32 @@ int main() {
     printf("Enter number of blocks: ");
     scanf("%d", &nb);
     printf("Enter block sizes: ");
-    for(int i=0;i<nb;i++) scanf("%d",&b[i]);
+    for(int i=0;i<nb;i++)
+        scanf("%d",&b[i]);
 
     printf("Enter number of processes: ");
     scanf("%d", &np);
     printf("Enter process sizes: ");
-    for(int i=0;i<np;i++){ scanf("%d",&p[i]); alloc[i]=-1; }
+    for(int i=0;i<np;i++){
+        scanf("%d",&p[i]);
+        alloc[i]=-1; }
 
     // Worst-Fit allocation
     for(int i=0;i<np;i++){
         int worst=-1;
         for(int j=0;j<nb;j++)
-            if(b[j]>=p[i] && (worst==-1 || b[j]>b[worst])) worst=j;
-        if(worst!=-1){ alloc[i]=worst; b[worst]-=p[i]; }
+            if(b[j]>=p[i] && (worst==-1 || b[j]>b[worst])) 
+                worst=j;
+        if(worst!=-1){
+            alloc[i]=worst;
+            b[worst]-=p[i]; 
+        }
     }
 
     printf("\nProcess\tSize\tBlock\n");
     for(int i=0;i<np;i++)
-        if(alloc[i]==-1) printf("%d\t%d\tNot Allocated\n", i+1, p[i]);
-        else printf("%d\t%d\t%d\n", i+1, p[i], alloc[i]+1);
+        if(alloc[i]==-1) 
+            printf("%d\t%d\tNot Allocated\n", i+1, p[i]);
+        else
+            printf("%d\t%d\t%d\n", i+1, p[i], alloc[i]+1);
 }
